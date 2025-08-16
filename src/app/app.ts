011ba8a5394e846ce89507565bcefd60c26cc67c
NavyaKtr <navi@yahoo.com>
2025-08-16 10:47:37 -0400

    AngularProject1

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
-})
+})     
 export class App {
   protected readonly title = signal('project1');
 }
