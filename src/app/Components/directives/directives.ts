a96d37e55b8f311787f443a1bebe96ae73689ab0
NavyaKtr <navi@yahoo.com>
2025-08-19 12:09:28 -0400

    Directives,ngClass, ndStyle, table, @for

--- src/app/Components/directives/directives.ts
+++ src/app/Components/directives/directives.ts
@@ -4,26 +4,79 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-directives',
-  imports: [FormsModule,
-    CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './directives.html',
-  styleUrl: './directives.css'
+  styleUrl: './directives.css',
 })
 export class Directives {
+  flag: boolean = false;
   employees = [
-    { "eId": 101, "name": "sanjay", "sal": 5000, "gender": "male" },
-    { "eId": 104, "name": "geeta", "sal": 8000, "gender": "female" },
-    { "eId": 103, "name": "sameer", "sal": 7000, "gender": "male" },
-    { "eId": 102, "name": "sita", "sal": 9000, "gender": "female" },
-    { "eId": 105, "name": "deepak", "sal": 8000, "gender": "male" }
+    { eId: 101, name: 'sanjay', sal: 5000, gender: 'male' },
+    { eId: 104, name: 'geeta', sal: 8000, gender: 'female' },
+    { eId: 103, name: 'sameer', sal: 7000, gender: 'male' },
+    { eId: 102, name: 'sita', sal: 9000, gender: 'female' },
+    { eId: 105, name: 'deepak', sal: 8000, gender: 'male' },
   ];
 
   employees1 = [
-  { id: 1, name: 'Rathan Tata', role: 'Manager', salary: 85000, status: 'Active', img: 'Rathan Tata.jpeg' },
-  { id: 2, name: 'Sathya Nadella', role: 'Developer', salary: 45000, status: 'Active', img: 'SathyaNadella.jpeg' },
-  { id: 3, name: 'subroto bagchi', role: 'Tester', salary: 30000, status: 'Inactive', img: 'subroto bagchi.jpg' },
-  { id: 4, name: 'Azim Premji', role: 'HR', salary: 40000, status: 'Active', img: 'AzimPremji.jpg' }
-];
+    {
+      id: 1,
+      name: 'Rathan Tata',
+      role: 'Manager',
+      salary: 85000,
+      status: 'Active',
+      img: 'Rathan Tata.jpeg',
+    },
+    {
+      id: 2,
+      name: 'Sathya Nadella',
+      role: 'Developer',
+      salary: 45000,
+      status: 'Active',
+      img: 'SathyaNadella.jpeg',
+    },
+    {
+      id: 3,
+      name: 'subroto bagchi',
+      role: 'Tester',
+      salary: 30000,
+      status: 'Inactive',
+      img: 'subroto bagchi.jpg',
+    },
+    {
+      id: 4,
+      name: 'Azim Premji',
+      role: 'HR',
+      salary: 40000,
+      status: 'Active',
+      img: 'AzimPremji.jpg',
+    },
+  ];
 
-// images = ['staffing (2).png', 'staffing (2).png', 'staffing (2).png', 'staffing (2).png'];
+  // images = ['staffing (2).png', 'staffing (2).png', 'staffing (2).png', 'staffing (2).png'];
+
+  salStyle = {
+    color: 'green'
+  }
+
+  salStyle1 = {
+    color: 'red'
+  }
+
+  getButtonClass(emp: String) {
+
+    if(emp === 'Manager') {
+      return 'bg-warning p-3';
+    } else if (emp === 'Developer') {
+      return 'bg-info p-3';
+    } else if(emp === 'Tester') {
+      return 'bg-danger bg-opacity-25 p-3';
+    } else {
+      return '';
+    }
+  }
+
+  badgeStyle(sal: Number) {
+    return (+sal > 70000) ? "badge bg-success" : ((+sal >= 40000 && +sal <= 70000) ? "badge bg-warning" : ((+sal < 40000) ? "badge bg-danger" : ""));
+  }
 }
